3357c3bada8590cdde44c8b29e79a417fd73176d
Enzoss100 <lorenzossantillan@gmail.com>
2025-03-15 23:20:32 +0800

    updated init.lua and added more options to options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -3,7 +3,8 @@ require "nvchad.options"
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
-
+vim.opt.number = true
+vim.opt.relativenumber = true
 -- add yours here!
 
 -- local o = vim.o
